8fa046f2b6b5791a6610327ad2647c2573a723bb
DinhNgocThai <73975309+dinhngocthai@users.noreply.github.com>
2020-12-22 23:12:28 +0700

    Updaate mail, connect database

---
+++ src/main/java/demo/db/ConnectionDB.java
@@ -0,0 +1,37 @@
+package demo.db;
+
+import demo.beans.Product;
+import demo.entity.ProductEntity;
+import demo.filter.Data;
+
+import java.sql.*;
+import java.util.List;
+
+public class ConnectionDB {
+    static Connection con;
+
+    public static Statement connect() throws ClassNotFoundException, SQLException {
+        if (con == null || con.isClosed()) {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test1?useUnicode=true&characterEncoding=utf-8", "root", "");
+            return con.createStatement();
+        } else {
+            return con.createStatement();
+        }
+    }
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        ProductEntity pe= new ProductEntity();
+        pe.insertProduct(Data.data.values()) ;
+
+
+/*        Statement s = ConnectionDB.connect();
+        ResultSet rs = s.executeQuery("select * from user ");
+        rs.last();
+        System.out.println(rs.getRow());
+        rs.beforeFirst();
+        while (rs.next()) {
+            System.out.println(rs.getString(2));
+        }*/
+    }
+}
